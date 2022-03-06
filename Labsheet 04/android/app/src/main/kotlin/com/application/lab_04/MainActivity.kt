47c3c26421ebd6fb22c0bac4ff6071c853707fb2
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2022-03-06 11:22:10 +0530

    lab 4 completed

---
+++ Labsheet 04/android/app/src/main/kotlin/com/application/lab_04/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.application.lab_04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Labsheet 04/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lab_04/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Agify",
+          ),
+        ),
+        body: const Home(),
+      ),
+    );
+  }
+}
